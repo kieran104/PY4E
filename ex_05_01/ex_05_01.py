d3216d5ab169675fb8425dfd7c4849e0236efb91
KIERAN_WILLIAMS <kieran.deepsea@gmail.com>
2023-07-13 07:25:17 +0100

    number stuff

--- ex_05_01/ex_05_01.py
+++ ex_05_01/ex_05_01.py
@@ -1,1 +1,20 @@
-print('Test')
+numberCount = 0
+numberTotal = 0
+
+while True:
+    userNumber = input('Enter a number:')
+    if userNumber == 'done':
+        break
+    try:
+        userNumberFloat = float(userNumber)
+    except:
+        print('Invalid input')
+        continue
+    
+    numberTotal = userNumberFloat + numberTotal
+    numberCount = numberCount + 1
+
+print('Average:' , numberTotal / numberCount)
+print('Sum of all numbers:', numberTotal)
+print('Count of numbers:', numberCount)
+    
